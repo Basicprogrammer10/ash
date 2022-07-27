e63cf3db726be2898ab6b82dbaf2c52c93ca824c
Connor Slade <connor@connorcode.com>
2022-07-27 16:30:53 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::fs;
 
-use bitvec::{order::Lsb0, vec::BitVec, view::BitView};
 use hound::{SampleFormat, WavReader, WavSpec, WavWriter};
 
+mod decode;
 mod encode;
 
 fn main() {
-    // write();
+    write();
     read();
 }
 
@@ -27,24 +27,12 @@ fn write() {
 }
 
 fn read() {
+    let mut decoder = decode::BinDecoder::new();
     let mut reader = WavReader::open("./out.wav").unwrap();
-    let samples = reader
+    reader
         .samples::<f32>()
         .map(|x| x.unwrap())
-        .collect::<Vec<_>>();
+        .for_each(|x| decoder.add(x));
 
-    let mut i = 1;
-    let mut start = 0;
-    let mut data = BitVec::<u8, Lsb0>::new();
-    while i < samples.len() {
-        let value = samples[i];
-        if value < 0. && samples[i - 1] >= 0. {
-            data.push(i - start > 50);
-            start = i;
-        }
-
-        i += 1;
-    }
-
-    fs::write("out.jpg", data.into_vec()).unwrap();
+    fs::write("out.jpg", decoder.done()).unwrap();
 }
